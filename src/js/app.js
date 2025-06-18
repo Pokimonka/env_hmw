b0503c1279c39f0c03be67a36fa63cf1555acc31
Pokimonka <alenkaponomareva7@gmail.com>
2025-06-19 01:46:42 +0300

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,5 @@
 // TODO: write code here
 
-import Example from "./Example";
-
 // comment this to pass build
 const unusedVariable = "variable";
 
@@ -11,13 +9,3 @@ export default function demo(value) {
 }
 
 console.log("app.js included");
-
-const root = document.querySelector(".root");
-
-setTimeout(() => {
-  root.textContent = "content";
-}, 4000);
-
-let exm = new Example(root);
-
-exm.change("text from change");
